5a2dc8b0c0efa0484b620504f5ab76616fe7fd4f
maidang1 <497350746@qq.com>
2023-07-22 23:51:18 +0800

    feat: add node midware

--- functions/index.js
+++
@@ -1,7 +0,0 @@
-import { handler as ssrHandler } from "../dist/server/entry.mjs"
-
-// export function handler(event) {
-//   return ssrHandler(event);
-// }
-
-console.log(typeof ssrHandler)
---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { handler as ssrHandler } from './dist/server/entry.mjs';
+
+const app = express();
+app.use(express.static('dist/client/'));
+app.use((req, res, next) => {
+  const locals = {
+    title: 'New title',
+  };
+
+  ssrHandler(req, res, next, locals);
+});
+
+app.listen({
+  port:80,
+  host:"0.0.0.0",
+});
